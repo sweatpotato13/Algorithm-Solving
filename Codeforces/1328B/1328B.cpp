0b3f587e5ac96133b1331c27428fef8fde5532ed
Jang Chorong <crjang@tracom.kr>
2020-04-06 11:07:30 +0900

    Solve Codeforces 1328B

---
+++ Codeforces/1328B/1328B.cpp
@@ -0,0 +1,29 @@
+#pragma warning(disable : 4996)
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+using namespace std;
+vector<ll> ans;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+ 
+	int t;
+	cin >> t;
+	while(t--){
+		int n,k;
+		cin >> n >> k;
+		string s(n, 'a');
+		for(int i = n-2; i>=0;i--){
+			if (k <= (n - i - 1)) {
+                s[i] = 'b';
+                s[n - k] = 'b';
+                cout << s << endl;
+                break;
+            }
+            k -= (n - i - 1);
+		}
+	}
+ 
+	return 0;
+}
