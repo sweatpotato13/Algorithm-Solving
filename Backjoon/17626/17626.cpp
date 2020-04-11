9beacb239ec4cbb1c86ae33aa7fb53e112f67bbb
Jang Chorong <sweatpotato13@gmail.com>
2020-04-12 07:50:03 +0900

    Solve Backjoon 17626

---
+++ Backjoon/17626/17626.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+using namespace std;
+int dp[50001]={0,};
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int i;
+	int n;
+	cin >> n;
+	vector<int> v(227);
+	for(int i = 0;i<227;i++)
+		v.push_back(i*i);
+	for(int i = 0;i<=n;i++){
+		dp[i] = 2100000000;
+	}		
+	dp[0] = 0;
+	dp[1] = 1;
+	for(int i = 2;i<=n;i++){
+		int min = 2100000000;
+		for(int j = 0;v[j]<=i;j++){
+			min = std::min(min, dp[i-v[j]]);
+		}
+		dp[i] = min+1;
+	}
+	cout << dp[n] << "\n";
+
+	return 0;
+}
