995da1256e9f9e5e28c5a0fcf274a5b008b894b5
JangChorong <sweatpotato13@gmail.com>
2020-10-09 23:02:26 +0900

    Solve Some Backjoon Problem

---
+++ Backjoon/1550/1550.cpp
@@ -0,0 +1,26 @@
+#pragma warning(disable : 4996)
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef tuple<ll,ll,ll> tl3;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define rep(i, n) FOR(i, 0, n)
+#define repn(i, n) FORN(i, 1, n)
+#define tc(t) while (t--)
+// https://www.acmicpc.net/problem/1550
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+    cin >> uppercase >> hex >> n;
+    cout << n; 
+
+    return 0;
+}
