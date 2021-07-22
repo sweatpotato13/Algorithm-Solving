b7747d50653dae79527f7aef293a8bc067c4f049
JangChorong <sweatpotato13@gmail.com>
2021-07-22 19:35:27 +0900

    AtCoder Beginner Contest 206 Submissions (Virtual participation)

---
+++ AtCoder/abc206/abc206_b.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable : 4996)
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef tuple<ll, ll, ll> tl3;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define rep(i, n) FOR(i, 0, n)
+#define repn(i, n) FORN(i, 1, n)
+#define tc(t) while (t--)
+// https://atcoder.jp/contests/abc206/tasks/abc206_b
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for(int i = 1;;i++){
+        ans += i;
+        if(ans >= n){
+            cout << i;
+            return 0;
+        }
+    }
+
+
+    return 0;
+}
